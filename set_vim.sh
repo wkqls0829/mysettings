ddd3faedc4cced44a2efc9eb3189740d6bdebca0
Jabin <wkqls0829@gmail.com>
2020-11-26 19:49:35 +0900

    set_vim_v0.01

---
+++ set_vim.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+echo "Starting vim installation..."
+
+#removing vim-tiny and installing vim
+sudo apt remove -y vim-tiny
+sudo apt install -y vim-nox
+
+#install vim vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#set color 256
+setenv TERM xterm-256color
+
+#install ycmd settings
+sudo apt install -y build-essential cmake python3-dev
+sudo apt install -y clang
+
+#copy .vimrc
+cp ~/mysettings/.vimrc ~/
+vim +PluginInstall +qall
+git submodule update --init --recursive
+
+#compile vundle source
+~/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
+
+#get configuration
+git clone https://github.com/ycm-core/ycmd.git
+cp /ycmd/.ycm_extra_conf.py ~/.vim/
