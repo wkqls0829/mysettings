41cb604ede88e0502cda807421325156c003c67f
Jabin Koo <wkqls0829@gmail.com>
2021-03-31 21:47:38 +0900

    docker add

---
+++ set_docker.sh
@@ -0,0 +1,16 @@
+#installs docker settings
+
+#install dependancies
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+#add GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint oEBFCD88
+
+#register repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+
+#install docker
+sudo apt-get install -y docker-ce
