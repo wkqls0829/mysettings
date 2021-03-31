d1e6ca66907498c8dbc8a86e6ccc855ac6e04f55
Jabin Koo <wkqls0829@gmail.com>
2021-03-31 22:16:06 +0900

    docker error changed

--- set_docker.sh
+++ set_docker.sh
@@ -1,16 +1,24 @@
-#installs docker settings
+# installs docker settings
 
-#install dependancies
+# install dependancies
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-#add GPG key
+# add GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint oEBFCD88
 
-#register repository
+# register repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 
-#install docker
+# install docker
 sudo apt-get install -y docker-ce
+
+# set permissions
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+newgrp docker
+
+# test docker
+docker run hello-world
